5a8f813214e232523e377115543d235b34e1bb55
ivanovnp <coolworld2049@gmail.com>
2023-11-10 07:52:39 +0300

    exec_hql.sh

--- big_data/src/pr3_map_reduce_etl/exec_hql.sh
+++ big_data/src/pr3_map_reduce_etl/exec_hql.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-USER="$(whoami)"
-QUERY=$(cat "$1")
-sudo docker exec -it hiveserver2-standalone beeline -u jdbc:hive2://hiveserver2-standalone:10000/ -n $USER --color=true -e "$QUERY"
+set -eou pipefail
+
+if [ -f "$1" ]; then
+  QUERY=$(cat "$1")
+else
+  QUERY="$1"
+fi
+
+sudo docker exec -it hiveserver2-standalone beeline -u jdbc:hive2://hiveserver2-standalone:10000/ -n "$(whoami)" --color=true -e "$QUERY"
