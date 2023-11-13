2be1809072c1d07ae82e44c61fed406294d7c138
coolworld2049 <coolworld2049@gmail.com>
2023-11-13 06:20:19 +0300

    upd

--- big_data/src/hadoop_docker/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker compose up -d
-docker compose cp hadoop:/home/hduser/hadoop-3.3.3/bin/hdfs $HOME/hadoop-3.3.3
-export HADOOP_HOME=$HOME/hadoop-3.3.3
-export PATH=$PATH:$HADOOP_HOME/bin
-source ~/.bashrc
