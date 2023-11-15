faf54a681f57fbbb89ec2b157cae9eafb9c2ea0c
coolworld2049 <coolworld2049@gmail.com>
2023-11-16 00:47:53 +0300

    pr2_tsp_symmetric

--- data_warehouse/src/pr2_tsp_symmetric/__main__.py
+++ data_warehouse/src/pr2_tsp_symmetric/__main__.py
@@ -1,78 +1,131 @@
-import numpy as np
+import random
+
+import networkx as nx
+from loguru import logger
 
 
-class ChargedSystemSearch:
-    def __init__(self, obj_func, dim, num_agents, iterations, lower_bound, upper_bound):
-        self.obj_func = obj_func
-        self.dim = dim
-        self.num_agents = num_agents
-        self.iterations = iterations
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
+class DistanceCalculator:
+    def __init__(self, graph):
+        self.graph = graph
 
-    def run(self):
-        positions = np.random.uniform(
-            self.lower_bound, self.upper_bound, (self.num_agents, self.dim)
-        )
-        charges = np.random.uniform(-1, 1, self.num_agents)
-
-        for _ in range(self.iterations):
-            fitness_values = np.array(
-                [self.obj_func(position) for position in positions]
-            )
-            best_index = np.argmin(fitness_values)
-            best_position = positions[best_index]
-            best_fitness = fitness_values[best_index]
-
-            charges = charges * np.exp(-fitness_values)
-
-            for i in range(self.num_agents):
-                for j in range(self.dim):
-                    rand1, rand2 = np.random.rand(), np.random.rand()
-                    positions[i, j] = (
-                        best_position[j]
-                        + rand1 * (self.upper_bound - self.lower_bound) * charges[i]
-                    )
-                    positions[i, j] = max(
-                        self.lower_bound, min(self.upper_bound, positions[i, j])
-                    )
-
-        return best_position, best_fitness
+    def calculate_distance(self, route):
+        distance = 0
+        for i in range(len(route) - 1):
+            distance += self.graph.edges[route[i], route[i + 1]]["weight"]
+        distance += self.graph.edges[route[-1], route[0]]["weight"]
+        return distance
 
 
-# Example usage:
-def objective_function(x):
-    return x[0] ** 2 + x[1] ** 2
+class RouteInitializer:
+    def __init__(self, graph):
+        self.graph = graph
+
+    def initialize_route(self):
+        route = list(self.graph.nodes)
+        random.shuffle(route)
+        return route
 
 
-def sphere_function(x):
-    return np.sum(x**2)
+class ParentSelector:
+    def __init__(self, tournament_size, distance_calculator):
+        self.tournament_size = tournament_size
+        self.distance_calculator = distance_calculator
+
+    def select_parents(self, population):
+        tournament = random.sample(population, self.tournament_size)
+        return min(tournament, key=self.distance_calculator.calculate_distance)
 
 
-def rosenbrock_function(x):
-    return np.sum(100.0 * (x[1:] - x[:-1] ** 2) ** 2 + (1 - x[:-1]) ** 2)
+class CrossoverOperator:
+    def crossover(self, parent1, parent2):
+        size = len(parent1)
+        start, end = sorted([random.randint(0, size - 1) for _ in range(2)])
+        temp = parent1[start:end] + [
+            city for city in parent2 if city not in parent1[start:end]
+        ]
+        return temp[start:] + temp[:start]
 
 
-def rastrigin_function(x):
-    A = 10
-    return A * len(x) + np.sum(x**2 - A * np.cos(2 * np.pi * x))
+class MutationOperator:
+    def mutate(self, individual):
+        idx1, idx2 = random.sample(range(len(individual)), 2)
+        individual[idx1], individual[idx2] = individual[idx2], individual[idx1]
+        return individual
 
 
-dimension = 2
-num_particles = 20
-num_iterations = 10
-lower_bound = -5.0
-upper_bound = 5.0
+class GeneticAlgorithm:
+    def __init__(
+        self,
+        population_initializer,
+        parent_selector,
+        crossover_operator,
+        mutation_operator,
+        distance_calculator,
+    ):
+        self.population_initializer = population_initializer
+        self.parent_selector = parent_selector
+        self.crossover_operator = crossover_operator
+        self.mutation_operator = mutation_operator
+        self.distance_calculator = distance_calculator
 
-css = ChargedSystemSearch(
-    objective_function,
-    dimension,
-    num_particles,
-    num_iterations,
-    lower_bound,
-    upper_bound,
+    def run(self, pop_size, generations, crossover_prob, mutation_prob):
+        population = [
+            self.population_initializer.initialize_route() for _ in range(pop_size)
+        ]
+
+        for gen in range(generations):
+            new_population = []
+
+            for _ in range(pop_size // 2):
+                parent1 = self.parent_selector.select_parents(population)
+                parent2 = self.parent_selector.select_parents(population)
+
+                if random.random() < crossover_prob:
+                    child1 = self.crossover_operator.crossover(parent1, parent2)
+                    child2 = self.crossover_operator.crossover(parent2, parent1)
+                else:
+                    child1, child2 = parent1[:], parent2[:]
+
+                if random.random() < mutation_prob:
+                    child1 = self.mutation_operator.mutate(child1)
+                if random.random() < mutation_prob:
+                    child2 = self.mutation_operator.mutate(child2)
+
+                new_population.extend([child1, child2])
+
+            population = new_population
+
+        best_route = min(population, key=self.distance_calculator.calculate_distance)
+        return best_route
+
+
+# Create a random graph representing cities and distances
+G = nx.complete_graph(10)
+for edge in G.edges():
+    G.edges[edge]["weight"] = random.randint(1, 10)
+
+# Create instances of the classes
+distance_calculator = DistanceCalculator(G)
+route_initializer = RouteInitializer(G)
+parent_selector = ParentSelector(
+    tournament_size=3, distance_calculator=distance_calculator
 )
-best_solution, best_fitness = css.run()
+crossover_operator = CrossoverOperator()
+mutation_operator = MutationOperator()
 
-print("Best Solution:", best_solution)
-print("Best Fitness:", best_fitness)
+# Create an instance of the GeneticAlgorithm
+genetic_algorithm = GeneticAlgorithm(
+    population_initializer=route_initializer,
+    parent_selector=parent_selector,
+    crossover_operator=crossover_operator,
+    mutation_operator=mutation_operator,
+    distance_calculator=distance_calculator,
+)
+
+# Run the genetic algorithm
+best_route = genetic_algorithm.run(
+    pop_size=100, generations=1000, crossover_prob=0.7, mutation_prob=0.2
+)
+
+logger.info(f"Best Route: {best_route}")
+logger.info(f"Total Distance: {distance_calculator.calculate_distance(best_route)}")
