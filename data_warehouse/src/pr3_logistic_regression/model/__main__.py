443480ade5e2555a0935dfcd78f0afda6bdd7186
coolworld2049 <coolworld2049@gmail.com>
2023-12-03 06:33:56 +0300

    pr3_logistic_regression Test Accuracy: 80.21%

--- data_warehouse/src/pr3_logistic_regression/model/__main__.py
+++ data_warehouse/src/pr3_logistic_regression/model/__main__.py
@@ -1,51 +1,29 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import classification_report
+import plotly.express as px
+from numpy import ndarray
+from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import plotly.express as px
+
+from pr3_logistic_regression.model.logreg import LogisticRegression
 
 
-class LogisticRegression:
-    def __init__(self, learning_rate=0.01, iterations=1000):
-        self.learning_rate = learning_rate
-        self.iterations = iterations
-        self.theta = None
-
-    @staticmethod
-    def sigmoid(z):
-        return 1 / (1 + np.exp(-z))
-
-    def fit(self, X, y):
-        X = np.c_[np.ones((X.shape[0], 1)), X]
-        self.theta = np.zeros(X.shape[1])
-
-        for _ in range(self.iterations):
-            h = self.sigmoid(X @ self.theta)
-            gradient = X.T @ (h - y) / len(y)
-            self.theta -= self.learning_rate * gradient
-
-    def predict(self, X, threshold=0.5):
-        X = np.c_[np.ones((X.shape[0], 1)), X]
-        return (self.sigmoid(X @ self.theta) >= threshold).astype(int)
-
-
-def preprocess_data(df):
+def preprocess_data(df, test_size=0.25) -> tuple[ndarray, ndarray, ndarray, ndarray]:
     X = StandardScaler().fit_transform(df.drop("Outcome", axis=1))
-    return train_test_split(X, df["Outcome"], test_size=0.2, random_state=42)
+    y = df["Outcome"]
+    return train_test_split(X, y, test_size=test_size, random_state=0)
 
 
-def train_and_evaluate(model: LogisticRegression, X_train, X_test, y_train, y_test):
+def train_and_evaluate(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    print(f"Test Accuracy: {np.mean(y_test == y_pred) * 100:.2f}%\n")
-    print(f"classification_report\n")
-    print(classification_report(y_test, y_pred))
+    print(f"Test Accuracy: {np.mean(y_pred == y_test) * 100:.2f}%\n")
+    # print(f"classification_report\n{classification_report(y_test, y_pred)}")
 
 
 if __name__ == "__main__":
     df = pd.read_csv("../input/diabetes.csv")
-
     correlation_matrix = df.corr()
     fig = px.imshow(
         correlation_matrix,
@@ -57,10 +35,17 @@ if __name__ == "__main__":
     fig.update_layout(title="Correlation Matrix Heatmap", width=800, height=600)
     # fig.show()
 
-    print("Train and evaluate logistic regression model\n")
+    print("1. Train and evaluate sklearn logistic regression model\n")
+    train_and_evaluate(linear_model.LogisticRegression(), *preprocess_data(df))
+
+    print("2. Train and evaluate logistic regression model\n")
     train_and_evaluate(LogisticRegression(), *preprocess_data(df))
 
-    print("Train and evaluate logistic regression model with selected features\n")
-    drop_columns = ["BloodPressure", "SkinThickness", "BMI", "Insulin"]
+    print("3. Train and evaluate logistic regression model with selected features\n")
+    drop_columns = [
+        "BloodPressure",
+        "SkinThickness",
+        "Age",
+    ]
     clean_df = df.drop(drop_columns, axis="columns")
     train_and_evaluate(LogisticRegression(), *preprocess_data(clean_df))
